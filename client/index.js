179e3effd275b8c654d8f6beca2be1211448280b
brijesh agarwal <brijesh@jumpbyte.com>
2018-05-17 09:51:55 +0530

    update

--- client/index.js
+++ client/index.js
@@ -20,6 +20,10 @@ function tconfig($routeProvider, $locationProvider) {
 function tcontroller($scope, $http) {
 
   $scope.player = {};
+  $scope.selectedTeam = "";
+  $scope.teams = [];
+
+  $scope.currentBid = null;
 
   $scope.getPlayer = function(id) {
     $http.get(`/api/players/id/${id}`)
@@ -28,6 +32,28 @@ function tcontroller($scope, $http) {
     });
   }
 
+  $scope.getTeams = function() {
+    $http.get('/api/teams?filter=%7B%22include%22%3A%20%22players%22%7D')
+    .then(team => {
+      $scope.teams = team.data
+      $scope.teams.map(t => {
+        t.leftBudget = t.budget;
+        return t;
+      })
+    });
+  }
+
   $scope.getPlayer("1");
+  $scope.getTeams();
+
+  $scope.isBidding = function (index) {
+    const bid = $scope.currentBid ? $scope.currentBid + 100 : $scope.player.price;
+    if (+$scope.selectedTeam.team_id === (index + 1)) return;
+    if ($scope.teams[index].leftBudget - bid < 0) return;
+
+    $scope.selectedTeam = $scope.teams[index];
+    $scope.currentBid = bid;
+    $scope.teams[index].budget =  $scope.teams[index].leftBudget - $scope.currentBid;
+  }
 
 }
